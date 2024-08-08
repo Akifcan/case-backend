e3e9bfb827155a0f350558ad9cad9df5a0784c71
Akifcan <akfkara97@gmail.com>
2024-08-09 00:51:54 +0300

    feat(auth): auth module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { UserModule } from './modules/user/user.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { DbConfig } from './config/config.interface'
+import { AuthModule } from './modules/auth/auth.module'
 import configuration from './config/configuration'
 
 @Module({
@@ -28,8 +27,7 @@ import configuration from './config/configuration'
       },
     }),
     UserModule,
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
