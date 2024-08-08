242bf38e2ba7351262614186bf88aef1a4bf512e
Akifcan <akfkara97@gmail.com>
2024-08-09 00:54:56 +0300

    build(validator): validator setup

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { AppConfig } from './config/config.interface'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get<ConfigService>(ConfigService)
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(configService.get<AppConfig>('app').port)
 }
 bootstrap()
