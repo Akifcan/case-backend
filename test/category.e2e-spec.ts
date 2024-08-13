acf803e9343cd1a4559e27cfa9c211240def2b5f
Akifcan <akfkara97@gmail.com>
2024-08-13 04:05:20 +0300

    test(e2e): added category tests

---
+++ test/category.e2e-spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from '../src/app.module'
+
+describe('Category Controller (e2e)', () => {
+  let app: INestApplication
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    await app.init()
+  })
+
+  it('/category (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/category')
+    expect(response.statusCode).toBe(200)
+  })
+})
